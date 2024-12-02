7bf007b9cffdbc9e087a5f5e2b7467bb05756bdc
IngenieroJaimer <feriaamayajaimer21@gmail.com>
2024-12-02 08:17:02 -0500

    Add files via upload

---
+++ javascript/Laboratorio1.js
@@ -0,0 +1,34 @@
+//**laboratorio1-parte 2 */
+let a=6;
+let b=2;
+
+let suma=a+b;
+let resta=a-b;
+let multi=a*b;
+let divi=a/b;
+
+console.log("suma; "+suma);
+console.log("resta; "+resta);
+console.log("multiplicacion; "+multi);
+console.log("divison; "+divi);
+
+//**laboratorio1-parte 1 */
+let nombre="Jaimer";
+let edad=25;
+let esEstudiante=false;
+
+console.log("nombre; "+nombre);
+console.log("edad; "+edad);
+console.log("Es estudiante; "+esEstudiante);
+
+//**labotatorio1-parte 3 */
+console.log('laboratorio 1 - ejercicio 3');
+let esMayorDeEdas = false;
+let tienelicencia = true;
+
+if ( esMayorDeEdas == true && tienelicencia == true)
+    console.log('la persona puede conducir');
+else
+    console.log('la persona "No" puede conducir');
+
+console.log(`es mayor de edad: ${esMayorDeEdas}`);
